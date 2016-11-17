96c4eb9dd3142eb2b2241f40bb7a90670951b847
Lourenco Lima da Cunha <llc.from.hell@gmail.com>
2016-11-17 01:51:29 +0000

    corrigindo merge

--- estoque51/resources/views/produto/listagem.blade.php
+++ estoque51/resources/views/produto/listagem.blade.php
@@ -14,7 +14,6 @@
 
 <div class="container-fluid">
 
-<<<<<<< HEAD
   <h1>Listagem de produtos</h1>
   <table class="table table-striped table-bordered table-hover">
   
@@ -57,46 +56,6 @@
   
   @endif
 
-=======
-    <h1>Listagem de produtos</h1>
-    
-    <table class="table table-striped table-bordered table-hover">
-
-    @foreach ($produtos as $p)
-    <tr class="{{ $p->quantidade <= 1 ? 'danger' : ''}}">
-        <td>{{ $p->nome }}</td>
-        <td>{{ $p->valor }}</td>
-        <td>{{ $p->descricao }}</td>
-        <td>{{ $p->quantidade }}</td>
-        <td>
-            <a href="/produtos/mostra/{{ $p->id }}">
-                <span class="glyphicon glyphicon-search"></span>
-            </a>
-        </td>
-
-    </tr>
-    @endforeach
-
-    </table>
-
-</div>
-
-<div class="container-fluid">
-  <h4>
-  <span class="label label-danger pull-right">
-    Um ou menos itens no estoque
-  </span>
-  </h4>
-</div>
-
-    @if ( old('nome') )
-    <div class="container-fluid">
-        <div class="alert alert-success">
-            Produto {{ old('nome') }} foi adicionado com sucesso!
-        </div>
-    </div>
-    @endif
->>>>>>> 72eb0f49846aeb7ea7cde0fd6c37fe0140d2d795
 
 @endif
 
